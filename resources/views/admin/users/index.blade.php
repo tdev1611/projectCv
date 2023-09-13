30831dd0117b6bfce6a93d047b15ff71ad321268
Tdev1611-Hai <haitd1611@gmail.com>
2023-09-14 01:03:47 +0700

    Initial Commit

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,159 @@
+@extends('admin.layout')
+@section('content')
+    <style>
+        #delete:hover {
+            color: red;
+            padding: 5px
+        }
+
+        #update:hover {
+            color: greenyellow;
+            padding: 5px
+        }
+    </style>
+
+    <div class="d-flex flex-column flex-column-fluid">
+        <!--begin::Toolbar-->
+        <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
+            <!--begin::Toolbar container-->
+            <div id="kt_app_toolbar_container" class="app-container container-fluid d-flex flex-stack">
+                <!--begin::Page title-->
+                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+                    <!--begin::Title-->
+                    <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
+                        Thành viên</h1>
+                    <!--end::Title-->
+                    <!--begin::Breadcrumb-->
+                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">
+                            <a href="{{ route('admin.home') }}" class="text-muted text-hover-primary">Home</a>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">Danh sách</li>
+                        <!--end::Item-->
+                    </ul>
+                    <!--end::Breadcrumb-->
+                    {{-- alert --}}
+
+
+                </div>
+            </div>
+            <!--end::Toolbar container-->
+            {{-- alert --}}
+            {{-- component alert --}}
+            <x-admin.alert-notification />
+        </div>
+        <div class="lg-12 py-3 container">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+
+                            <div class="card-body">
+                                <h4 class="card-title">Danh sách thành viên</h4>
+                                <div class="table-responsive">
+                                    <table id="myTable"
+                                        class="table table-hover table-row-dashed table-row-gray-300 gy-7 table-striped">
+                                        <thead>
+                                            <tr class="fw-bold fs-6 text-gray-800 px-7">
+                                                <th>#</th>
+                                                <th>Tên</th>
+                                                <th>Điện thoại</th>
+                                                <th>Mã giới thiệu</th>
+                                                <th>Mã được mời</th>
+                                                <th>Số tiền</th>
+                                                <th>Vai trò</th>
+                                                <th>Trạng thái</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+
+    @section('js')
+        {{-- <script>
+            $(document).ready(function() {
+                $('#myTable').DataTable({
+                    order: [
+                        [0, 'desc']
+                    ],
+                    "processing": true,
+                    "serverSide": true,
+                    ajax: "{{ route('admin.users.data') }}",
+                    "columns": [{
+                            "data": "id"
+                        },
+                        {
+                            "data": "name"
+                        },
+                        {
+                            "data": "phone"
+                        },
+                        {
+                            "data": "referral_code"
+                        },
+                        {
+                            "data": "referrer_code"
+                        },
+                        {
+                            "data": "balance"
+                        },
+                        {
+                            "data": "role",
+                            "render": function(data, type, full, meta) {
+                                if (data === 1) {
+                                    return "<span class =''>Thành viên</span>";
+                                } else if (data === 2) {
+                                    return "<span class ='badge bg-primary '>Admin</span>";
+                                }
+                            }
+                        },
+                        {
+                            "data": "status",
+                            "render": function(data, type, full, meta) {
+                                if (data === 1) {
+                                    return "<span class ='badge bg-success'>Hoạt động</span>";
+                                } else if (data === 2) {
+                                    return "<span class ='badge bg-danger'>Vô hiệu hóa</span>";
+                                } else {
+                                    return 'Khác';
+                                }
+                            }
+
+                        },
+
+                        {
+                            "data": "id",
+
+                            "render": function(data, type, full, meta) {
+                                var editLink = '{{ route('admin.users.edit', ':id') }}';
+                                var deleteLink = '{{ route('admin.users.delete', ':id') }}';
+                                editLink = editLink.replace(':id', data);
+                                deleteLink = deleteLink.replace(':id', data);
+                                return `<a href=" ${editLink}"> <i id="update" class="fa-solid fa-pen-to-square"></i></a> / <a href=" ${deleteLink}" >  <i id="delete" class="fa-solid fa-delete-left"></i></a>`;
+                            }
+                            // ${deleteLink } 
+                        },
+                    ]
+                });
+            });
+        </script> --}}
+    @endsection
---
+++ resources/views/components/admin/app-sidebar-menu.blade.php
@@ -0,0 +1,254 @@
+<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true"
+    data-kt-scroll-activate="true" data-kt-scroll-height="auto"
+    data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer"
+    data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+
+    <!--begin::Menu website-->
+
+    <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true"
+        data-kt-menu-expand="false">
+        <!--begin:Menu item-->
+        <div class="menu-item pt-5">
+            <!--begin:Menu content-->
+            <div class="menu-content">
+                <span class="menu-heading fw-bold text-uppercase fs-7">Quản lý cài đặt website</span>
+            </div>
+            <!--end:Menu content-->
+        </div>
+        <!-- module -->
+        <!--begin:Menu notify-->
+        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+            <!--begin:Menu link-->
+            <span class="menu-link">
+                <span class="menu-icon">
+                    {{-- begin::Svg Icon | path: icons/duotune/communication/com011.svg --}}
+                    <span class="svg-icon svg-icon-2">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path opacity="0.3"
+                                d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z"
+                                fill="currentColor" />
+                            <path
+                                d="M21 5H2.99999C2.69999 5 2.49999 5.10005 2.29999 5.30005L11.2 13.3C11.7 13.7 12.4 13.7 12.8 13.3L21.7 5.30005C21.5 5.10005 21.3 5 21 5Z"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                    <!--end::Svg Icon-->
+                </span>
+                <span class="menu-title">
+                    <h5 style="color:gray">Cài đặt trang</h5>
+                </span>
+                <span class="menu-arrow"></span>
+            </span>
+            <!--end:Menu link-->
+            <!--begin:Menu sub-->
+            <div class="menu-sub menu-sub-accordion">
+                <!--begin:Menu item-->
+                <div class="menu-item">
+                    <!--begin:Menu link-->
+                    <a class="menu-link" href="">
+                        <span class="menu-bullet">
+                            <span class="bullet bullet-dot"></span>
+                        </span>
+                        <span class="menu-title">Setting </span>
+                    </a>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+
+                <!--begin:Menu item-->
+
+                <!--end:Menu item-->
+            </div>
+            <!--end:Menu sub-->
+        </div>
+        {{-- end notify --}}
+        <!--begin:Menu notify-->
+        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+            <!--begin:Menu link-->
+            <span class="menu-link">
+                <span class="menu-icon">
+                    {{-- begin::Svg Icon | path: icons/duotune/communication/com011.svg --}}
+                    <span class="svg-icon svg-icon-2">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path opacity="0.3"
+                                d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z"
+                                fill="currentColor" />
+                            <path
+                                d="M21 5H2.99999C2.69999 5 2.49999 5.10005 2.29999 5.30005L11.2 13.3C11.7 13.7 12.4 13.7 12.8 13.3L21.7 5.30005C21.5 5.10005 21.3 5 21 5Z"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                    <!--end::Svg Icon-->
+                </span>
+                <span class="menu-title">
+                    <h5 style="color:gray">Cài đặt thông báo</h5>
+                </span>
+                <span class="menu-arrow"></span>
+            </span>
+            <!--end:Menu link-->
+            <!--begin:Menu sub-->
+            <div class="menu-sub menu-sub-accordion">
+                <!--begin:Menu item-->
+                <div class="menu-item">
+                    <!--begin:Menu link-->
+                    <a class="menu-link" href="">
+                        <span class="menu-bullet">
+                            <span class="bullet bullet-dot"></span>
+                        </span>
+                        <span class="menu-title">Danh sách thông báo </span>
+                    </a>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+
+                <!--begin:Menu item-->
+
+                <!--end:Menu item-->
+            </div>
+            <!--end:Menu sub-->
+        </div>
+        {{-- end notify --}}
+
+
+        <!-- module -->
+    </div>
+    <!--end::Menu website-->
+
+
+    <!--begin::Menu-->
+    <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true"
+        data-kt-menu-expand="false">
+        <!--begin:Menu item-->
+        <div class="menu-item pt-5">
+            <!--begin:Menu content-->
+            <div class="menu-content">
+                <span class="menu-heading fw-bold text-uppercase fs-7">Quản lý</span>
+            </div>
+            <!--end:Menu content-->
+        </div>
+        <!-- module -->
+        <!--begin:Menu item-->
+        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+            <!--begin:Menu link-->
+            <span class="menu-link">
+                <span class="menu-icon">
+                    {{-- begin::Svg Icon | path: icons/duotune/communication/com011.svg --}}
+                    <span class="svg-icon svg-icon-2">
+                        <i class="fa-brands fa-product-hunt"></i>
+                    </span>
+                    <!--end::Svg Icon-->
+                </span>
+                <span class="menu-title">
+                    <h5 style="color:gray">Thành viên</h5>
+                </span>
+                <span class="menu-arrow"></span>
+            </span>
+            <!--end:Menu link-->
+            <!--begin:Menu sub-->
+            <div class="menu-sub menu-sub-accordion">
+
+
+                <!--begin:Menu item-->
+                <div class="menu-item">
+                    <!--begin:Menu link-->
+                    <a class="menu-link" href="{{ route('admin.users.index') }}">
+                        <span class="menu-bullet">
+                            <span class="bullet bullet-dot"></span>
+                        </span>
+                        <span class="menu-title">Danh sách thành viên</span>
+                    </a>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+            </div>
+            <!--end:Menu sub-->
+        </div>
+        <!--end:Menu item user-->
+
+
+
+
+
+
+        <!--begin:Menu item-->
+        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+            <!--begin:Menu link-->
+            <span class="menu-link">
+                <span class="menu-icon">
+                    {{-- begin::Svg Icon | path: icons/duotune/communication/com011.svg --}}
+                    <span class="svg-icon svg-icon-2">
+                        <i class="fa-brands fa-product-hunt"></i>
+                    </span>
+                    <!--end::Svg Icon-->
+                </span>
+                <span class="menu-title">
+                    <h5 style="color:gray">Danh sách rút tiền</h5>
+                </span>
+                <span class="menu-arrow"></span>
+            </span>
+            <!--end:Menu link-->
+            <!--begin:Menu sub-->
+            <div class="menu-sub menu-sub-accordion">
+
+                <!--begin:Menu item-->
+                <div class="menu-item">
+                    <!--begin:Menu link-->
+                    <a class="menu-link" href="">
+                        <span class="menu-bullet">
+                            <span class="bullet bullet-dot"></span>
+                        </span>
+                        <span class="menu-title">Rút tiền</span>
+                    </a>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+            </div>
+            <!--end:Menu sub-->
+        </div>
+        <!--end:Menu item-->
+
+
+        <!--begin:DEpossti-->
+        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+            <!--begin:Menu link-->
+            <span class="menu-link">
+                <span class="menu-icon">
+                    {{-- begin::Svg Icon | path: icons/duotune/communication/com011.svg --}}
+                    <span class="svg-icon svg-icon-2">
+                        <i class="fa-brands fa-product-hunt"></i>
+                    </span>
+                    <!--end::Svg Icon-->
+                </span>
+                <span class="menu-title">
+                    <h5 style="color:gray">Danh sách nạp tiền</h5>
+                </span>
+                <span class="menu-arrow"></span>
+            </span>
+            <!--end:Menu link-->
+            <!--begin:Menu sub-->
+            <div class="menu-sub menu-sub-accordion">
+
+                <!--begin:Menu item-->
+                <div class="menu-item">
+                    <!--begin:Menu link-->
+                    <a class="menu-link" href="">
+                        <span class="menu-bullet">
+                            <span class="bullet bullet-dot"></span>
+                        </span>
+                        <span class="menu-title">Nạp tiền</span>
+                    </a>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+            </div>
+            <!--end:Menu sub-->
+        </div>
+        <!--end:DEpossti item-->
+
+
+        <!-- module -->
+    </div>
+    <!--end::Menu-->
+</div>
